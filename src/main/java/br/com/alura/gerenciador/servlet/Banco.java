cf452fe0b4439b1473b1d8b5e4ed2a4a890b9a26
marcb <marcbitenl@gmail.com>
2022-05-24 20:07:07 -0300

    Listando empresas com servlets

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -6,9 +6,19 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		lista.add(empresa);
+		lista.add(empresa2);
+	}
+	
 
 	public void adciona(Empresa empresa) {
-		Banco.lista;.add(empresa);
+		Banco.lista.add(empresa);
 	}
 	
 	public List<Empresa> getEmpresas(){
