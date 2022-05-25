0125f21f42c5ba4fee83a39307f630fab580026b
marcb <marcbitenl@gmail.com>
2022-05-25 20:03:02 -0300

    JSP para listar empresas

--- src/main/java/br/com/alura/gerenciador/servlet/ListasEmpresasServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/ListasEmpresasServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,17 +26,10 @@ public class ListasEmpresasServlet extends HttpServlet {
 		
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
-		PrintWriter out = response.getWriter();
+		request.setAttribute("empresas", lista);
 		
-		out.println("<html><body>");
-		out.println("<ul>");
-		
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		
-		out.println("</ul>");
-		out.println("</html></body>");
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response); 
 
 	}
 
